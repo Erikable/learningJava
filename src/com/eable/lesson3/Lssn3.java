99d80a6daee2fa825a40586ed80cea23875dcc81
Erikable <evsurmilo@gmail.com>
2021-02-06 22:21:48 +0300

    Lessn3 2/2 done

--- src/com/eable/lesson3/Lssn3.java
+++ src/com/eable/lesson3/Lssn3.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class Lssn3 {
     public static void main(String[] args) {
-        work1();
+        //work1();
+        //work2();
     }
     //  Написать программу, которая загадывает случайное число от 0 до 9 и пользователю дается 3 попытки угадать это число.
     //   При каждой попытке компьютер должен сообщить, больше ли указанное пользователем число, чем загаданное, или меньше.
@@ -43,7 +44,9 @@ public class Lssn3 {
 //     String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
 //          "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
 //          "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"}.
-//   При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя, сравнивает его с загаданным словом и сообщает, правильно ли ответил пользователь. Если слово не угадано, компьютер показывает буквы, которые стоят на своих местах.
+//   При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя,
+//     сравнивает его с загаданным словом и сообщает, правильно ли ответил пользователь.
+//     Если слово не угадано, компьютер показывает буквы, которые стоят на своих местах.
 //   apple – загаданное
 //   apricot - ответ игрока
 //   ap############# (15 символов, чтобы пользователь не мог узнать длину слова)
@@ -52,5 +55,43 @@ public class Lssn3 {
 //   char a = str.charAt(0); - метод вернет char, который стоит в слове str на первой позиции
 //   Играем до тех пор, пока игрок не отгадает слово.
 //   Используем только маленькие буквы.
+
+    static void work2(){
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
+                            "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
+                                "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        Scanner sc = new Scanner(System.in);
+        Random rndm = new Random();
+        boolean isFinish = false;
+        int numOfWords = words.length;
+        int randomWord = rndm.nextInt(numOfWords);
+        String secretWord = words[randomWord];
+        char[] hashes = new char[15];
+        for (int i = 0; i < 15; i++) {
+            hashes[i] = '#';
+        }
+        System.out.printf(" Hint to check method !!! secret word is ( %s )\n", secretWord);
+        while (!isFinish) {
+            String myAnswer = sc.nextLine();
+            if (myAnswer.equals(""))
+                break;
+            if (myAnswer.equals(secretWord)) {
+                System.out.println("WIN");
+                isFinish = true;
+                continue;
+            }
+            for (int i = 0; i < ( (secretWord.length() < myAnswer.length())? secretWord.length() : myAnswer.length() ); i++) {
+                if (i >= myAnswer.length())
+                    break;
+                if (secretWord.charAt(i) == myAnswer.charAt(i)) {
+                    hashes[i] = myAnswer.charAt(i);
+                }
+            }
+            for (int i = 0; i < 15; i++) {
+                System.out.print(hashes[i]);
+            }
+            System.out.println("");
+        }
+    }
 }
 
